0eb1fd29b4fb10f88ea87e0118880fe75bd8342a
Javier Ramirez <jramir11@bancoestado.cl>
2022-11-02 17:28:51 -0300

    actualizado

--- heapifyMonticulos.js
+++ heapifyMonticulos.js
@@ -1,34 +1,41 @@
 //Convertir un array en un montículo
 function heapify(element){
-    let indice_hijo = 0;
-    let temp=0;
+    //crear monticulo inicial
+    let temp=element[0];
+    element[0]=undefined;
+    element.push(temp);
 
     let nuevoHeap=[];
-    nuevoHeap.push(undefined);
-
-    for(let i=0;i<element.length;i++){
-
-        //insertando el nuevo valor en el array heap
+    for (let i = 0; i < element.length; i++) {
         nuevoHeap.push(element[i]);
-
-        //indice del nuevo elemento
-        indice_hijo = nuevoHeap.length - 1;
-        indice_padre = Math.trunc(indice_hijo / 2);
-
-        //compara padre e hijo e invierte valores si padre es mayor
-        while (nuevoHeap[indice_padre] > nuevoHeap[indice_hijo]){
-            //console.log("1111",indice_padre,indice_hijo,nuevoHeap[indice_padre] , nuevoHeap[indice_hijo]," - ",nuevoHeap);
-            temp = nuevoHeap[indice_hijo];
-            nuevoHeap[indice_hijo] = nuevoHeap[indice_padre];
-            nuevoHeap[indice_padre] = temp;
-
-            indice_hijo=indice_padre;
-            indice_padre = Math.trunc(indice_hijo / 2);
-            //console.log("2222",indice_padre,indice_hijo,nuevoHeap[indice_padre] , nuevoHeap[indice_hijo]," - ",nuevoHeap);
-        }
     }
+
+   
+    //heapify de nuevo monticulo
+    let j = nuevoHeap.length - 1;
+    let idx_padre = Math.trunc(j / 2);
+    let idx_hijoMenor = idx_padre * 2;
+    let idx_hijoMayor = (idx_padre * 2) + 1;
+
+    while (idx_padre > 0) {
+        temp = nuevoHeap[idx_padre];
+
+        if (nuevoHeap[idx_padre] > nuevoHeap[idx_hijoMayor]) {
+                nuevoHeap[idx_padre] = nuevoHeap[idx_hijoMayor];
+                nuevoHeap[idx_hijoMayor] = temp;
+        }
+        if (nuevoHeap[idx_padre] > nuevoHeap[idx_hijoMenor]) {
+                nuevoHeap[idx_padre] = nuevoHeap[idx_hijoMenor];
+                nuevoHeap[idx_hijoMenor] = temp;
+        } 
+        idx_padre = idx_padre - 1;
+        idx_hijoMenor = idx_padre * 2;
+        idx_hijoMayor = (idx_padre * 2) + 1;
+    }
+
     return nuevoHeap;
 }
+
     // salidas de muestras
     nuevoHeap = heapify([20, 3, 8, 14, 9, 6, 2]); // res= [undefined, 2, 6, 3, 9, 8, 14, 20]
     console.log(nuevoHeap);
